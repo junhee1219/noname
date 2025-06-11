5ae18cccdbeef321a0e45e8ee3874e2e3cd27466
JHLee <leejunhee1219@snu.ac.kr>
2025-06-11 22:14:32 +0900

    first commit : 환경설정.

---
+++ src/main/java/io/github/junhee1219/amugeona/AmugeonaApplication.java
@@ -0,0 +1,13 @@
+package io.github.junhee1219.amugeona;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmugeonaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmugeonaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/junhee1219/amugeona/AmugeonaApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.junhee1219.amugeona;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmugeonaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
